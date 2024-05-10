efc3f8d2ea2fdc7d665603e9e6fb7bbb810a632c
Maikel101 <miguel.rol@outlook.es>
2024-05-10 22:15:47 +0200

    Commit

---
+++ SistemaVenta.DAT/Repositorios/Contrato/IVentaRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SistemaVenta.Model;
+
+namespace SistemaVenta.DAT.Repositorios.Contrato
+{
+    public interface IVentaRepository : IGenericRepository<Venta>
+    {
+        Task<Venta> Registrar(Venta modelo);
+
+    }
+}
